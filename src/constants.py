460a897980abd19bfe4d1aef32609b488d765a78
henrique-britoleao <hbl451@gmail.com>
2022-02-09 15:07:02 +0100

    Added feature engineering

--- src/constants.py
+++ src/constants.py
@@ -5,10 +5,13 @@ from datetime import datetime
 import os
 
 # paths
-DATA_PATH = "../data"
+DATA_PATH = "data"
 INPUT_DATA_PATH = os.path.join(DATA_PATH, "inputs", "transactions_dataset.csv")
 VALIDATION_DATA_PATH = os.path.join(DATA_PATH, "validation", "val_transactions.csv")
 TRAIN_TRANSACTION_PATH = os.path.join(DATA_PATH, "train", "train_transactions.csv")
+FEATURES_PATH = os.path.join(DATA_PATH, "train", "features.csv")
+TRAIN_TARGET_PATH = os.path.join(DATA_PATH, "train", "training_labels.csv")
+VAL_TARGET_PATH = os.path.join(DATA_PATH, "validation", "validation_labels.csv")
 
 # Data metadata
 MAX_DATE = datetime.strptime("2019-09-22", "%Y-%m-%d")
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,11 +4,15 @@
 import pandas as pd
 import constants as cst
 from datetime import timedelta
+from functools import partial
+import numpy as np
 
 #####  Get logger
 import logging
 
 logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
 
 #####  Preprocessor
 class Preprocessor:
@@ -17,13 +21,21 @@ class Preprocessor:
 
     def preprocess(
         self,
-        data: pd.DataFrame,
+        data: pd.DataFrame = None,
         train_val_split: bool = True,
         val_split_lenght: int = 90,
     ) -> pd.DataFrame:
-        data = self.enforce_data_quality(data)
         if train_val_split:
+            data = self.enforce_data_quality(data)
             self.get_validation_set(data, lenght=val_split_lenght)
+
+        # load train_data
+        data = pd.read_csv(cst.TRAIN_TRANSACTION_PATH, index_col=0)
+        data["date_order"] = pd.to_datetime(data["date_order"])
+        data = data.sort_values("date_order")
+
+        data = self.feature_engineering(data)
+        data.to_csv(cst.FEATURES_PATH)
 
         raise NotImplementedError()
 
@@ -57,3 +69,82 @@ class Preprocessor:
         val_set.to_csv(cst.VALIDATION_DATA_PATH)
 
         logger.info("Done saving validation and train transactions")
+
+    def feature_engineering(self, data: pd.DataFrame) -> pd.DataFrame:
+        logger.info("Started feature engineering")
+
+        features = data.groupby(by="client_id", as_index=False).agg(
+            online_percent=("order_channel", partial(item_freq, item="online")),
+            phone_percent=("order_channel", partial(item_freq, item="by phone")),
+            store_percent=("order_channel", partial(item_freq, item="at the store")),
+            visit_percent=(
+                "order_channel",
+                partial(item_freq, item="during the visit of a sales rep"),
+            ),
+            other_percent=("order_channel", partial(item_freq, item="other")),
+            mean_qty=("quantity", "mean"),
+            max_qty=("quantity", "max"),
+            min_qty=("quantity", "min"),
+            std_qty=("quantity", "std"),
+            last_qty_1=("quantity", partial(last, n=1)),
+            last_qty_2=("quantity", partial(last, n=2)),
+            last_qty_3=("quantity", partial(last, n=3)),
+            last_qty_4=("quantity", partial(last, n=4)),
+            mean_sales=("sales_net", "mean"),
+            max_sales=("sales_net", "max"),
+            min_sales=("sales_net", "min"),
+            std_sales=("sales_net", "std"),
+            last_sales_1=("sales_net", partial(last, n=1)),
+            last_sales_2=("sales_net", partial(last, n=2)),
+            last_sales_3=("sales_net", partial(last, n=3)),
+            last_sales_4=("sales_net", partial(last, n=4)),
+            n_branch=("branch_id", "nunique"),
+            n_product=("product_id", "nunique"),
+            purchase_freq=("date_order", purchase_frequency),
+            delay_purchase_n1=("date_order", partial(time_delay, n=1)),
+            delay_purchase_n2=("date_order", partial(time_delay, n=2)),
+            delay_purchase_n3=("date_order", partial(time_delay, n=3)),
+            delay_purchase_n4=("date_order", partial(time_delay, n=4)),
+            client_age=("date_order", partial(time_from_today, n=0)),
+            time_from_last_purchase=("date_order", partial(time_from_today, n=-1)),
+            client_lifetime=("date_order", lifetime),
+        )
+
+        logger.info("Done feature engineering")
+
+        return features
+
+
+def purchase_frequency(dates):
+    return pd.Timedelta(np.diff(dates.unique()).mean()).total_seconds() / (60 * 60 * 24)
+
+
+def time_delay(dates, n=1):
+    try:
+        return pd.Timedelta(dates.iloc[-n] - dates.iloc[-n - 1]).total_seconds() / (
+            60 * 60 * 24
+        )
+    except IndexError:
+        return np.nan
+
+
+def lifetime(dates):
+    return pd.Timedelta(dates.iloc[-1] - dates.iloc[0]).total_seconds() / (60 * 60 * 24)
+
+
+def time_from_today(dates, n=0):
+    return pd.Timedelta(cst.MAX_DATE - dates.iloc[n]).total_seconds() / (60 * 60 * 24)
+
+
+def last(sequence, n=1):
+    try:
+        return sequence.iloc[-n]
+    except IndexError:
+        return np.nan
+
+
+def item_freq(series, item):
+    if not np.isin(item, series):
+        return 0
+    else:
+        return series.value_counts(normalize=True)[item]
