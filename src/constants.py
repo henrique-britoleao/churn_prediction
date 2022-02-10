9ebcdd00a82a2d5b04fdde7a3990f476d1a09b9a
henrique-britoleao <hbl451@gmail.com>
2022-02-10 13:40:46 +0100

    Added labels to preprocessing

--- src/constants.py
+++ src/constants.py
@@ -12,6 +12,13 @@ TRAIN_TRANSACTION_PATH = os.path.join(DATA_PATH, "train", "train_transactions.cs
 FEATURES_PATH = os.path.join(DATA_PATH, "train", "features.csv")
 TRAIN_TARGET_PATH = os.path.join(DATA_PATH, "train", "training_labels.csv")
 VAL_TARGET_PATH = os.path.join(DATA_PATH, "validation", "validation_labels.csv")
+TRAIN_SET_PATH = os.path.join(DATA_PATH, "train", "train_set.csv")
+TEST_SET_PATH = os.path.join(DATA_PATH, "test", "test_set.csv")
+FULL_PREDICTIONS_PATH = os.path.join(DATA_PATH, "outputs", "predictions.csv")
+SHAP_VALUES_PATH = os.path.join(DATA_PATH, "outputs", "shap_values.npy")
+EXPLAINER_PATH = os.path.join(DATA_PATH, "outputs", "explainer.pickle")
+PIPELINE_PATH = os.path.join(DATA_PATH, "outputs", "pipeline.pickle")
+
 
 # Data metadata
 MAX_DATE = datetime.strptime("2019-09-22", "%Y-%m-%d")
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,11 +1,13 @@
 ###############################################################################
 #####  Data Preprocesssing
 ###############################################################################
+from typing import Tuple
 import pandas as pd
 import constants as cst
 from datetime import timedelta
 from functools import partial
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 #####  Get logger
 import logging
@@ -16,18 +18,17 @@ logger.setLevel(logging.DEBUG)
 
 #####  Preprocessor
 class Preprocessor:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, val_split_length: int = 90) -> None:
+        self.split_day = cst.MAX_DATE - timedelta(val_split_length)
 
     def preprocess(
         self,
         data: pd.DataFrame = None,
         train_val_split: bool = True,
-        val_split_lenght: int = 90,
     ) -> pd.DataFrame:
         if train_val_split:
             data = self.enforce_data_quality(data)
-            self.get_validation_set(data, lenght=val_split_lenght)
+            self.get_validation_set(data)
 
         # load train_data
         data = pd.read_csv(cst.TRAIN_TRANSACTION_PATH, index_col=0)
@@ -37,7 +38,12 @@ class Preprocessor:
         data = self.feature_engineering(data)
         data.to_csv(cst.FEATURES_PATH)
 
-        raise NotImplementedError()
+        labels = self.get_labels(data)
+        train_set, test_set = self.get_train_test_sets(
+            pd.read_csv(cst.FEATURES_PATH, index_col=0), labels
+        )
+
+        return train_set, test_set
 
     def enforce_data_quality(self, data: pd.DataFrame) -> pd.DataFrame:
         # remove zero/negative quantities
@@ -57,11 +63,9 @@ class Preprocessor:
 
         return data
 
-    def get_validation_set(self, data: pd.DataFrame, lenght: int) -> None:
-        split_day = cst.MAX_DATE - timedelta(lenght)
-
-        train_set = data.loc[data["date_order"] < split_day]
-        val_set = data.loc[data["date_order"] >= split_day]
+    def get_validation_set(self, data: pd.DataFrame) -> None:
+        train_set = data.loc[data["date_order"] < self.split_day]
+        val_set = data.loc[data["date_order"] >= self.split_day]
 
         logger.info("Done splitting validation and train transactions")
 
@@ -113,6 +117,76 @@ class Preprocessor:
         logger.info("Done feature engineering")
 
         return features
+
+    def get_labels(self, data: pd.DataFrame) -> pd.DataFrame:
+        # get purchasing frequencies
+        freqs = data.groupby(by="client_id", as_index=False).agg(
+            last_purchase=("date_order", "max"),
+            frequency=("date_order", purchase_frequency),
+        )
+
+        # setting churn deffinitions
+        freq_churn = self.split_day - timedelta(30)
+        medium_churn = self.split_day - timedelta(90)
+
+        # assiging client categories
+        freqs.loc[
+            (freqs["frequency"] > 0) & (freqs["frequency"] < 10), "client_category"
+        ] = "freq_buyer"
+        freqs.loc[
+            (freqs["frequency"] >= 10) & (freqs["frequency"] < 32), "client_category"
+        ] = "med_buyer"
+        freqs.loc[
+            (freqs["frequency"] >= 32) | (freqs["frequency"] == 0), "client_category"
+        ] = "infreq_buyer"
+
+        # assigning labels
+        freqs.loc[
+            (freqs["client_category"] == "freq_buyer")
+            & (freqs["last_purchase"] <= freq_churn),
+            "is_churn",
+        ] = 1
+        freqs.loc[
+            (freqs["client_category"] == "freq_buyer")
+            & (freqs["last_purchase"] > freq_churn),
+            "is_churn",
+        ] = 0
+        freqs.loc[
+            (freqs["client_category"] == "med_buyer")
+            & (freqs["last_purchase"] <= medium_churn),
+            "is_churn",
+        ] = 1
+        freqs.loc[
+            (freqs["client_category"] == "med_buyer")
+            & (freqs["last_purchase"] > medium_churn),
+            "is_churn",
+        ] = 0
+
+        logger.info("Done getting labels")
+
+        freqs.to_csv(cst.TRAIN_TARGET_PATH)
+
+        return freqs
+
+    def get_train_test_sets(
+        self, training_data: pd.DataFrame, training_target: pd.DataFrame
+    ) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        full_training_data = pd.merge(
+            training_data, training_target, on="client_id", how="left"
+        )
+        full_training_data.dropna(inplace=True)
+        train_set, test_set = train_test_split(
+            full_training_data, test_size=0.3, random_state=42
+        )
+
+        logger.info("Done splitting train and test set")
+
+        train_set.to_css(cst.TRAIN_SET_PATH)
+        test_set.to_css(cst.TEST_SET_PATH)
+
+        logger.info("Done saving train and test set")
+
+        return train_set, test_set
 
 
 def purchase_frequency(dates):
